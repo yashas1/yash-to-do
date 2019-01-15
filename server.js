87320245a891f94f8a4272e1613d7d88977b1060
yashas <yashu.gsb@gmail.com>
2019-01-16 00:11:47 +0530

    updated todo

--- server.js
+++ server.js
@@ -3,6 +3,34 @@ var express = require('express');
 var auto =express();
 var port =process.env.PORT || 3000;
 
+var todo=[{id:1,description:"meet the ppl",completed:false},{
+    
+    id:2,description:"take the bath",completed:false},{id:3,description:"meet the friendsl",completed:true}
+   
+]
+
+auto.get('/todos/:id',function(req,res){
+    var todoid = parseInt(req.params.id);
+    var mathcedid;
+    
+    todo.forEach(function(todos){
+        
+        if(todoid==todos.id)
+{
+    mathcedid=todos; 
+    
+}
+        
+    })
+   res.json(mathcedid);
+    
+})
+
+auto.get('/todos',function(req,res){
+    
+    res.json(todo);
+    
+})
 
 auto.get('/',function(req,res){
     
